0878eacc1ca7bc1defa87d2c22b4f280fd0dd09d
evanchen2914 <evanchen2914@gmail.com>
2019-05-09 11:49:13 -0400

    add all item navigation

---
+++ lib/pages/all_items.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class AllItems extends StatefulWidget {
+  static const routeName = '/allItems';
+
+  AllItems({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return AllItemsState();
+  }
+}
+
+class AllItemsState extends State<AllItems> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () {
+        goBack();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('All items'),
+          // back button
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              goBack();
+            },
+          ),
+        ),
+        body: showBody(),
+        //floatingActionButton: showFAB(),
+      ),
+    );
+  }
+
+  Widget showBody() {
+    return Center(child: Text('Hello'),);
+  }
+
+  void goBack() {
+    Navigator.pop(context);
+  }
+}
