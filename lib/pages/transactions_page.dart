a969938b6a95f17726668b0eb0c18f3ef7135e7f
evanchen2914 <evanchen2914@gmail.com>
2019-11-21 17:41:27 -0500

    optimize imports

--- lib/pages/transactions_page.dart
+++ lib/pages/transactions_page.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shareapp/extras/helpers.dart';
 import 'package:shareapp/main.dart';
